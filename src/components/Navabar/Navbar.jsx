21b400e678b05f1c4a54cb4514a0558386d4d401
abdullahshafiq153 <abdullahshafiq153@gmail.com>
2024-06-14 04:13:33 +0500

    Cart Page Integration

--- src/components/Navabar/Navbar.jsx
+++ src/components/Navabar/Navbar.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import Cart from '../../pages/Cart/Cart';
+
 const Navbar = ({setShowLogin}) => {
 
   const [menu,setMenu]=useState("Home");
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt='logo' className='logo'/>
+        <Link to='/'><img src={assets.logo} alt='logo' className='logo'/></Link>
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
             <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</a>
@@ -16,7 +18,7 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt=''/>
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt=''/>
+                <Link to='/cart'><img src={assets.basket_icon} alt=''/></Link>
                 <div className="dot"></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>Sign In</button>
