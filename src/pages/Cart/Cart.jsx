c7d06ac5c522cef9feb30f29efec6f90db2c5cc8
abdullahshafiq153 <abdullahshafiq153@gmail.com>
2024-06-14 04:30:39 +0500

    Cart Items Integration

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useState } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext, useState } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
-  const {cartItems,food_list,removeFromCart}=useContext(StoreContext);
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -18,9 +18,26 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-      </div>  
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={()=>removeFromCart(item._id)} className="cross">x</p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
