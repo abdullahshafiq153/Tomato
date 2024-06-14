f70af8d8b61b722d120ac22155e164726316332b
abdullahshafiq153 <abdullahshafiq153@gmail.com>
2024-06-14 07:29:17 +0500

    Vercel Analytics bug testing 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
 
