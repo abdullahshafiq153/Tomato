86620a7d8adfdfb055806eefe85c10f9fb861fc4
abdullahshafiq153 <abdullahshafiq153@gmail.com>
2024-06-14 05:00:15 +0500

    Cart Page Integration Completed

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -20,9 +20,13 @@ const StoreContextProvider = (props) => {
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
-    for(const item in cartItems){
-        
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
+    return totalAmount;
   };
 
   const contextValue = {
@@ -31,6 +35,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
   return (
